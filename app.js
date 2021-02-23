88bd566ff71f499636901e8d54a4fe4feac85aff
benzed3 <benzed3@gmail.com>
2021-02-23 17:54:35 -0500

    buttons working and adding correct heads/tails image

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-$("button").on("click", function () {
-    alert("clicked!");
+$("#heads").on("click", function () {
+
+    flipCoin(0);
+
 })
+
+$("#tails").on("click", function () {
+
+    flipCoin(1);
+
+})
+
+function flipCoin(result) {
+
+    var randomNumber = Math.floor(Math.random() * 2);
+
+    if (randomNumber === 0) {
+
+        $("#imgDiv").html("<img src='images/us-quarter-front.jpg'>");
+    }
+
+    else {
+        $("#imgDiv").html("<img src='images/us-quarter-back.jpg'>")
+    }
+
+}
+
