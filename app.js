517a77cf9f96046834269cfe712b666e17a89d5b
benzed3 <benzed3@gmail.com>
2021-02-23 18:27:30 -0500

    code to add and display wins and losses added

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 $("#heads").on("click", function () {
 
     $("#imgDiv").html("<img src='images/us-quarter-front.jpg'>");
+    flipCoin(0);
 
 })
 
 $("#tails").on("click", function () {
 
-    $("#imgDiv").html("<img src='images/us-quarter-back.jpg'>")
+    $("#imgDiv").html("<img src='images/us-quarter-back.jpg'>");
+    flipCoin(1);
 
 })
+
+var wins = 0;
+var losses = 0;
 
 function flipCoin(result) {
 
     var randomNumber = Math.floor(Math.random() * 2);
 
-    if (randomNumber === 0) {
-
-
+    if (result === randomNumber) {
+        wins++;
+        $("#wins").html("<h3>" + wins + "</h3>");
     }
 
     else {
-
+        losses++;
+        $("#losses").html("<h3>" + losses + "</h3>");
     }
 
 }
